43e1a2c037140ee522aeb0bf5ade806d06304700
Matt Long <matt@crocodoc.com>
2013-01-30 21:57:34 +0000

    bump version to 0.1.1

---
+++ HISTORY.md
@@ -0,0 +1,7 @@
+## History
+
+### 0.1.1 (2013-01-30)
+* fixes required requests package version
+
+### 0.1.0 (2012-10-26)
+* initial release
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="crocodoc",
     packages=['crocodoc'],
-    version='0.1.0',
+    version='0.1.1',
     author="Crocodoc, Inc.",
     license="MIT",
     author_email="admin@crocodoc.com",
